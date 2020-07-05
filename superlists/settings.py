272e4f48411a753e4c7334ff867df4ac8db46949
Helen Glass <helencglass@Helens-MBP.fios-router.home>
2020-07-05 17:23:20 -0400

    debug on for quick troubleshooting

--- superlists/settings.py
+++ superlists/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if 'DJANGO_DEBUG_FALSE' in os.environ:
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
