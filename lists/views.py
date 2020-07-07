921b8214b395b52890f550d58597467b0ae90fa7
Helen Glass <helencglass@Helens-MBP.fios-router.home>
2020-07-06 22:16:36 -0400

    refactor hard coded URLs out of templates

--- lists/views.py
+++ lists/views.py
@@ -22,11 +22,11 @@ def view_list(request, list_id):
 
     return render(request, 'list.html', {'list': list_, "error": error})
 
-
-def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+#
+# def new_list(request):
+#     list_ = List.objects.create()
+#     Item.objects.create(text=request.POST['item_text'], list=list_)
+#     return redirect(f'/lists/{list_.id}/')
 
 
 def new_list(request):
