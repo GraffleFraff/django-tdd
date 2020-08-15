42dc3782ee0e8e9184fc3291be6ae29adddfc63f
Helen Glass <helencglass@Helens-MBP.fios-router.home>
2020-08-15 17:21:38 -0400

    Revert "trying to resolve syntax error for jenkins"
    This reverts commit 69b8dcae96d73b9127248e3522058be170b92086.

--- accounts/views.py
+++ accounts/views.py
@@ -12,10 +12,9 @@ def send_login_email(request):
     url = request.build_absolute_uri(
         reverse('login') + '?token=' + str(token.uid)
     )
-    message_body = 'Use this link to log in: \n\n{url}'.format(url=url)
     send_mail(
         'Your login link for Superlists',
-        message_body,
+        f'Use this link to log in: \n\n{url}',
         'noreply@superlists',
         [email])
     messages.success(
